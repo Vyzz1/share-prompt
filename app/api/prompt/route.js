9d15b90b4335cdca73b9245890849e9bbd1ab125
VyHuynh <vykhanghuynh@gmail.com>
2024-06-10 14:50:55 +0700

    update

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,14 @@
-import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
-export const GET = async (req) => {
+import { connectToDatabase } from "@utils/database";
+
+export const GET = async (request) => {
   try {
     await connectToDatabase();
 
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-store",
-      },
-    });
-  } catch (err) {
-    console.error(err);
-    return new Response(JSON.stringify(err), { status: 500 });
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,86 +1,90 @@
 "use client";
-import { useState, useEffect, useCallback } from "react";
+
+import { useState, useEffect } from "react";
+
 import PrompCard from "./PrompCard";
-import debounce from "lodash.debounce";
-const PrompCardList = ({ data, handleTagClick }) => {
+
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16 prompt_layout ">
-      {Array.isArray(data) &&
-        data.map((v) => (
-          <PrompCard
-            key={v._id}
-            handleTagClick={() => handleTagClick(v.tag)}
-            post={v}
-          />
-        ))}
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PrompCard key={post._id} post={post} handleTagClick={handleTagClick} />
+      ))}
     </div>
   );
 };
 
 const Feed = () => {
-  const [posts, setPosts] = useState([]);
-  const [prePost, setprePost] = useState(posts);
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/prompt", { cache: "no-store" });
-        const data = await response.json();
-        setPosts(data);
-        setprePost(data);
-      } catch (error) {
-        console.error("Error fetching prompts:", error);
-      }
-    };
-    fetchData();
+    fetchPosts();
   }, []);
 
-  const [searchText, setSearchText] = useState("");
-  const filterPost = useCallback(
-    debounce((keySearch) => {
-      if (keySearch.length >= 2) {
-        const filteredPosts = prePost.filter(
-          (post) =>
-            post.prompt.toLowerCase().includes(keySearch.toLowerCase()) ||
-            post.tag.toLowerCase().includes(keySearch.toLowerCase()) ||
-            post.creator.username
-              .toLowerCase()
-              .includes(keySearch.toLowerCase())
-        );
-        setPosts(filteredPosts);
-      } else {
-        setPosts(prePost);
-      }
-    }, 300),
-    [prePost]
-  );
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
 
-  useEffect(() => {
-    filterPost(searchText);
-  }, [searchText, filterPost]);
-
-  const handleSearch = (e) => {
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
     setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
   };
 
-  const handleTagClick = (tag) => {
-    setSearchText(tag);
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
   };
-  console.log(posts);
+
   return (
-    <section className="feed ">
+    <section className="feed">
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Nhập để tìm kiếm"
+          placeholder="Search for a tag or a username"
           value={searchText}
-          onChange={handleSearch}
+          onChange={handleSearchChange}
           required
-          className="search_input peer
-        "
+          className="search_input peer"
         />
       </form>
-      <PrompCardList handleTagClick={handleTagClick} data={posts} />
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
